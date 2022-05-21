e8055762b0eecfbd6d8603b1cd82d20f2b7398ae
DeepakSidhar <deepaksidhar@gmail.com>
2022-05-21 14:49:11 +0800

    Validating the Password

---
+++ Account.py
@@ -0,0 +1,54 @@
+class Account:
+	#Attributes of the class
+	username: str
+	password: str
+
+	#Constructor taking in the paramters username and passoword and setting the attributes above
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+
+
+	def checkPassLong(self):
+		if len(self.password) >= 8 and len(self.password) <=10 :
+			return True
+		else:
+			print("Password is not in range")
+			return False
+
+	def checkPassUpper(self):
+		for letter in self.password:
+			if letter.isupper():
+				return True
+		print("No upper case Characters in password:")
+		return False
+
+
+	def checkPassLower(self):
+		for letter in self.password:
+			if letter.islower():
+				return True
+		print("No lower case Characters in password:")
+		return False
+
+	def checkPassSpace(self):
+		for letter in self.password:
+			if letter == " ":
+				print("space detected in password:")
+				return False
+
+		return True
+
+	def checkPassSpecial(self):
+		special = ['!', '@', '#', '$', '%', '&', '*']
+		for letter in self.password:
+			if letter in special:
+				return True
+		print("No special  characters in password:")
+		return False
+
+	def validatePassword(self):
+		return self.checkPassUpper() \
+		       and self.checkPassLower() \
+		       and self.checkPassSpace() \
+		       and self.checkPassSpecial()
--- MediCentre.py
+++ MediCentre.py
@@ -1,52 +1,13 @@
-def checkPassLong(length):
-	if length >= 8 and length <=10 :
-		print(checkPassUpper(password))
-	else:
-		return("Password is not within range")
-	break
+#Import the Account class into main program
+from Account import Account
 
-def checkPassUpper(password):
-	upper_case_letters = [letter.isupper() for letter in password].count(True)
-	if upper_case_letters >=1 :
-		print(checkPassLower(password))
-	else:
-		return("Password does not have any capital")
-	break
-
-
-
-def checkPassLower(password):
-	lower_case_letters = [letter.islower() for letter in password].count(True)
-	if lower_case_letters >=1 :
-		print("lower_case_letters", lower_case_letters)
-		print(checkPassSpace(password))
-	else:
-		return("Password does not have any lower case")
-		break
-
-def checkPassSpace(password):
-	for letter in password:
-		if letter != " ":
-			print("No Space ", )
-			print(checkPassSpecial(password))
-		else:
-			print("space detected in password:")
-			break
-
-def checkPassSpecial(password):
-	special = ['!', '@', '#', '$', '%', '&', '*']
-	special_case_letters = 0
-	for letter in password:
-		if letter in special:
-			special_case_letters +1
-			print("Specail " , special_case_letters)
-
-
-while(True):
-	password = input("Please enter a Password  ")
-	length =len(password)
+while True:
+	#Create variable  user name and passeod
+	username = input("Please enter a username  ")
+	password = input("Please enter a password  ")
 	print("Please wait while we perform some checks !!")
-	print("Total Length", length)
-	print(checkPassLong(length))
-	break
+	#Create an account object  called a
+	a = Account(username, password)
+	a.validatePassword()
+
 
